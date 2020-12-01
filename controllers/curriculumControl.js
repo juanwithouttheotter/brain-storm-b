01cd6acc637172d8f4c004b3b91db4d587ea70f1
Juan Osorio <juan.michael.osorio@gmail.com>
2020-11-30 18:41:43 -0600

    added population refs and update

--- controllers/curriculumControl.js
+++ controllers/curriculumControl.js
@@ -88,3 +88,104 @@ exports.getAll = async (req, res) => {
         .catch(err => console.log(err));
 }
 
+exports.getCurriculumCreator = async (req, res) => {
+    await db.Curriculum.findById(req.params.id)
+        .populate("created_by")
+        .then((curriculumCreator, err) => {
+            if (err) {
+                return res
+                    .status(400)
+                    .json({
+                        success: false,
+                        error: err
+                    })
+                    .end()
+            }
+            if (!curriculumCreator) {
+                return res
+                    .status(404)
+                    .json({
+                        success: false,
+                        error: 'curriculum creator not found'
+                    })
+                    .end()
+            }
+            return res
+                .status(200)
+                .json({
+                    success: true,
+                    data: curriculumCreator
+                })
+                .end()
+        })
+        .catch(err => console.log(err));
+}
+
+exports.getLessons = async (req, res) => {
+    await db.Curriculum.findById(req.params.id)
+        .populate("created_by")
+        .then((lessons, err) => {
+            if (err) {
+                return res
+                    .status(400)
+                    .json({
+                        success: false,
+                        error: err
+                    })
+                    .end()
+            }
+            if (!lessons) {
+                return res
+                    .status(404)
+                    .json({
+                        success: false,
+                        error: 'curriculum creator not found'
+                    })
+                    .end()
+            }
+            return res
+                .status(200)
+                .json({
+                    success: true,
+                    data: lessons
+                })
+                .end()
+        })
+        .catch(err => console.log(err));
+}
+
+exports.update = async (req, res) => {
+    await db.Curriculum.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true },
+        (err, curriculum) => {
+            if (err) {
+                return res
+                    .status(404)
+                    .json({ err, message: 'Curriculum not found!' })
+                    .end()
+            }
+            curriculum
+                .then(() => {
+                    return res
+                        .status(200)
+                        .json({
+                            success: true,
+                            id: curriculum._id,
+                            message: 'Curriculum updated!'
+                        })
+                        .end()
+                })
+                .catch(error => {
+                    return res
+                        .status(404)
+                        .json({
+                            error,
+                            message: 'Curriculum not updated!'
+                        })
+                        .end()
+                })
+        })
+        .catch(err => console.log(err));
+}
